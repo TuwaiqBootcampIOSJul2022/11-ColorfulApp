87618ba3b13f8825fbb0af80c22b0e53794157be
Waad <Waadow@MacBook-Pro-alkhas-b-Waad.local>
2022-08-21 22:01:31 +0300

    ColorfulApp

---
+++ ColorsTabel/ColorsTabel/ViewController2.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController2.swift
+//  ColorsTabel
+//
+//  Created by Waad on 23/01/1444 AH.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    var selectedColor = Color(name: "", uiColor: UIColor.clear)
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      print(selectedColor)
+       
+    }
+    
+
+
+
+}
