0e4e95e9cd4b5ff64f50ba4ec3b3181bc7f87b1f
Farahalyousef1 <110400621+Farahalyousef1@users.noreply.github.com>
2022-08-22 13:34:30 +0300

    FarahAlyousef

---
+++ colorFullApp/colorFullApp/VC2.swift
@@ -0,0 +1,29 @@
+//
+//  VC2.swift
+//  colorFullApp
+//
+//  Created by Farah Alyousef on 24/01/1444 AH.
+//
+
+import UIKit
+
+class VC2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
